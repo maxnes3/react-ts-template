786cfcb4ab5ffba7715f1bc835f38baaf7d3754d
maxnes3 <draw_max@mail.ru>
2025-05-24 20:07:23 +0400

    feat: initialize React TypeScript template with Vite
    - Add package.json with dependencies and scripts for development, build, linting, and preview.
    - Create initial App component with basic structure and styling.
    - Set up main entry point for React application.
    - Include TypeScript configuration files for app and node environments.
    - Configure Vite with React plugin and path aliases for cleaner imports.

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, eslintConfigPrettier],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'prettier/prettier': [
+        'error',
+        {
+          printWidth: 80,
+          semi: true,
+          tabWidth: 2,
+          useTabs: false,
+          endOfLine: 'auto',
+          singleQuote: true,
+          arrowParens: 'always',
+          plugins: [],
+        }
+      ]
+    },
+  },
+)
