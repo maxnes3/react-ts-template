e55a35b27fd60200d103925df4c8ab9d7e36434e
maxnes3 <draw_max@mail.ru>
2025-05-25 13:05:53 +0400

    feat: update AuthService to handle user login response and improve layout styles across components

--- src/api/authService.ts
+++ src/api/authService.ts
@@ -4,12 +4,12 @@ import { axiosConfig } from './axiosConfig';
 export const AuthService = {
   loginFn: async (data: AuthRequestDto): Promise<AuthResponseDto | null> => {
     const { phone, password } = data;
-    const response = await axiosConfig.get<AuthResponseDto>('/users', {
+    const response = await axiosConfig.get<AuthResponseDto[]>('/users', {
       params: {
         phone,
         password,
       },
     });
-    return response.data ?? null;
+    return response.data[0] ?? null;
   },
 };
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,24 +2,28 @@ import { useNavigate } from 'react-router-dom';
 import { RoutesPahEnum } from '@/types';
 import { Button } from '../button';
 import { useState } from 'react';
-import FaviconImg from '@assets/icons/favicon.png';
+import { useAuthContext } from '@/hooks/auth';
 import BurgerMenuIcon from '@assets/icons/burger-menu.svg';
 import classes from './styles.module.scss';
 
 export const Header = () => {
+  const { account } = useAuthContext();
   const [isCollapse, setIsCollapse] = useState(false);
   const navigate = useNavigate();
 
   const handleFaviconClick = () => {
     navigate(RoutesPahEnum.HOME);
+    setIsCollapse((prev) => !prev);
   };
 
   const handleNewsClick = () => {
     navigate(RoutesPahEnum.NEWS);
+    setIsCollapse((prev) => !prev);
   };
 
   const handleDirectionsClick = () => {
     navigate(RoutesPahEnum.DIRECTIONS);
+    setIsCollapse((prev) => !prev);
   };
 
   const handleToggleClick = () => {
@@ -41,9 +45,10 @@ export const Header = () => {
     <header className={classes.header}>
       <div className={classes.container}>
         <Button className={classes.favicon} onClick={handleFaviconClick}>
-          <img src={FaviconImg} alt={FaviconImg} />
+          <img src="https://is.ulstu.ru/Images/logo_1_1.png" alt="logo" />
         </Button>
         <div className={classes.desktop}>{collapseButtons}</div>
+        {account && <p>{account.name}</p>}
         <Button
           className={classes.burgerMenuButton}
           onClick={handleToggleClick}
